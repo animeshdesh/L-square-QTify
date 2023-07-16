93a30d40c6c61adda3b96cb7aa06c3d6f3c8b06c
animeshdesh <animeshdesh.aa@gmail.com>
2023-07-16 22:14:09 +0530

    cardCarouselTopAlbums-qtify-takehome

--- qtify/src/components/Songslist/Songslist.jsx
+++ qtify/src/components/Songslist/Songslist.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Displaycard from "../Displaycard/Displaycard";
 import axios from "axios";
-import { Grid } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import "./Songs.modules.css";
 
-const albumNewApi = "https://qtify-backend-labs.crio.do/albums/new";
+const albumNewApi = "https://qtify-backend-labs.crio.do/albums/top";
 
 const Songslist = () => {
   const [Songs, setSongs] = useState([]);
+  const [ShowCarausol, setShowCarausol] = useState(true);
+  let box = document.querySelector(".product-container");
 
   const fetchData = async () => {
     try {
@@ -22,22 +24,79 @@ const Songslist = () => {
   useEffect(() => {
     fetchData();
   }, []);
+
+  const handleShowAll = () => {
+    setShowCarausol(!ShowCarausol);
+  };
+
+  const btnpressprev = () => {
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft - width;
+    console.log(width);
+  };
+
+  const btnpressnext = () => {
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft + width;
+    console.log(width);
+  };
+
   return (
     <div>
       <div className="main-section">
-        <button className="main-btn">Show All</button>
+        <Typography sx={{ fontFamily: "poppins", fontSize: "25px" }}>
+          Top Album
+        </Typography>
+        <Button
+          sx={{ color: "#34c94b", fontFamily: "poppins", fontSize: "20px" }}
+          onClick={handleShowAll}
+        >
+          Show All
+        </Button>
       </div>
-      <div className="songs-wrapper">
-        {Songs.map((song) => {
-          return (
-            <Displaycard
-              albumImage={song.image}
-              followers={song.follows}
-              title={song.title}
-            />
-          );
-        })}
-      </div>
+      {ShowCarausol ? (
+        <div>
+          <div className="product-carousel">
+            <button className="pre-btn" onClick={btnpressprev}>
+              <p>&lt;</p>
+            </button>
+            <button className="next-btn" onClick={btnpressnext}>
+              <p>&gt;</p>
+            </button>
+            <div className="product-container">
+              {Songs.map((song) => {
+                return (
+                  <Displaycard
+                    albumImage={song.image}
+                    followers={song.follows}
+                    title={song.title}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Grid
+          container
+          marginY="1rem"
+          paddingX="1rem"
+          spacing={2}
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          {Songs.map((song) => {
+            return (
+              <Grid item xs={1.6} md={1.6} key={song.id}>
+                <Displaycard
+                  albumImage={song.image}
+                  followers={song.follows}
+                  title={song.title}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
     </div>
   );
 };
